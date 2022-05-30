90197754e2f9b2868de2804235aeecc2f7947b6d
miriamdosantos <mii.santos@yahoo.com.br>
2022-05-30 21:03:01 +0100

    Finalizacao da main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,26 +4,62 @@ import projetokotlin.User
 //para fazer o login utilize, ou modifique na class
 // ja utilize a opçao login, e não a de cadastro
 fun main() {
-    var user1 = User("", "raian.guilherme", "77", "", "")
-    println(
-        "Olá!!!\n" +
-                "Bem vindo ao app Not + Hungry\n" +
-                "MENU\n" +
-                "1-Cadastro\n" +
-                "2-Login\n"
-    )
-    var resp = readln().toInt()
-    when (resp) {
-        1 -> {
-            println("***Cadastro de novo usuário***\n")
-            user1.cadastroUser()
-            user1.loginUser()
-        }
-        2 -> {
-            user1.loginUser()
+
+
+
+    val user1 = User("", "raian.guilherme", "77", "", "")
+
+
+
+
+    while(true){
+
+        println(
+            "Olá!!!\n" +
+                    "Bem vindo ao app Not + Hungry\n" +
+                    "MENU\n" +
+                    "1-Cadastro\n" +
+                    "2-Login\n"
+        )
+
+
+      val  resp = readln().toInt()
+
+        try {
+            when (resp) {
+                1 -> {
+                    println("***Cadastro de novo usuário***\n")
+                    user1.cadastroUser()
+                    user1.loginUser()
+                    break
+                }
+                2 -> {
+                    user1.loginUser()
+                    break
+                }
+                // miriam
+
+                else -> {
+
+                    println("Opcao invalida")
+
+                }
+            }
+        } catch (e: Exception) {
+            println("Por favor digite a opcao correta e nao insira valor vazio")
 
         }
+
     }
+    //(resp!=1 || resp !=2)
+
+
+// while (resp != 1 || resp != 2)
+// catch (e: Exception){
+// println("Digitar opcao correta, nao vazia e letras")
+//}
+
+//
     do {
         println("Bem vindo ao app Fome Zero")
         println(
@@ -35,15 +71,17 @@ fun main() {
                     "5-Informações sobre o app\n" +
                     "6-Deslogar"
         )
-        var aux = readln().toInt()
+      val aux   = readln().toInt()
 
         when (aux) {
             1 -> {
                 user1.doarNota()
 
             }
-            2 -> {
-                var listProdutos = mutableListOf<String>("1-Camiseta", "2-Boné", "3-Vouncher15%", "4-Caneca")
+
+            2
+            -> {
+                val listProdutos = mutableListOf<String>("1-Camiseta", "2-Boné", "3-Vouncher15%", "4-Caneca")
                 listProdutos.forEach {
                     println(it)
                 }
@@ -67,7 +105,7 @@ fun main() {
                 println("Opção inválida!")
             }
         }
-    } while (aux != 6)
+
+    }while (aux!=6)
 
 }
-
